a1ba67e9f1bb60494696e6054e383382cd1a0698
James Harmison <jharmison@redhat.com>
2021-02-01 11:26:30 -0500

    Added descriptions to proxy configuration

--- src/faros_config/proxy.py
+++ src/faros_config/proxy.py
@@ -2,14 +2,23 @@
 
 This module contains the configuration models for the proxy section.
 """
-from pydantic import BaseModel
-from typing import List
+from pydantic import BaseModel, Field
+from typing import List, Optional
 
 
 class ProxyConfig(BaseModel):
-    """The proxy config section model."""
+    """Configuration of the HTTP/S proxy for the cluster."""
 
-    http: str
-    https: str
-    noproxy: List[str]
-    ca: str
+    http: Optional[str] = Field(
+        description="The HTTP proxy endpoint"
+    )
+    https: Optional[str] = Field(
+        description="The HTTPS proxy endpoint"
+    )
+    noproxy: Optional[List[str]] = Field(
+        [],
+        description="The list of sites that shouldn't be proxied."
+    )
+    ca: Optional[str] = Field(
+        description="The HTTPS proxy endpoint's certificate authority."
+    )
